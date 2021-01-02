d6097854a563d50588ab2e5f9733659065dcc6ff
Angela Carraro <angela_carraro@hotmail.it>
2021-01-02 12:16:53 +0100

    Other tests for suggestedAccountNumbers()

--- src/test/java/dssc/assignment/bank/GenerateAccountNumberTest.java
+++ src/test/java/dssc/assignment/bank/GenerateAccountNumberTest.java
@@ -2,6 +2,8 @@ package dssc.assignment.bank;
 
 import org.junit.jupiter.api.Test;
 
+import java.net.URL;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -60,5 +62,38 @@ public class GenerateAccountNumberTest {
         }
         List<String> expected = Arrays.asList("711111111");
         assertEquals(expected, suggestedStrings);
+    }
+
+    @Test
+    void suggestionForAnotherNotValid() throws Exception {
+        Entry entry = new Entry(" _  _  _  _  _  _  _  _  _ ","|_||_||_||_||_||_||_||_||_|","|_||_||_||_||_||_||_||_||_|");
+        AccountNumber number = new AccountNumber(entry);
+        List<AccountNumber> suggested = number.suggestedAccountNumbers();
+        List<String> suggestedStrings = new ArrayList<>();
+        for (AccountNumber x: suggested) {
+            suggestedStrings.add(x.toString());
+        }
+        List<String> expected = Arrays.asList("888886888", "888888988", "888888880");
+        assertEquals(expected, suggestedStrings);
+    }
+
+    @Test
+    void suggestionForMultipleEntriesFromFile() throws Exception {
+        URL allOnesSingleEntry = BankOcrAcceptanceTest.class.getClassLoader().getResource("multipleEntriesWithMissing");
+        EntryReader reader = new EntryReader(Path.of(allOnesSingleEntry.toURI()));
+        List<Entry> entries = reader.readEntries();
+        List<AccountNumber> numbers = new ArrayList<>();
+        for(Entry entry: entries){
+            numbers.add(new AccountNumber(entry));
+        }
+        List<String> suggested = new ArrayList<>();
+        for(AccountNumber accountNumber: numbers) {
+            List<AccountNumber> generatedAccountNumbers = accountNumber.suggestedAccountNumbers();
+            for (AccountNumber x: generatedAccountNumbers) {
+                suggested.add(x.toString());
+            }
+        }
+        List<String> expected = Arrays.asList("899999999", "993999999", "999959999", "490867715");
+        assertEquals(expected, suggested);
     }
 }
