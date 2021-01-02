45ba781e3f785edf5462fc27189ea7627fe14368
Angela Carraro <angela_carraro@hotmail.it>
2021-01-02 11:37:54 +0100

    Testing suggestedAccountNumber() and bug fixing

---
+++ src/test/java/dssc/assignment/bank/GenerateAccountNumberTest.java
@@ -0,0 +1,38 @@
+package dssc.assignment.bank;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GenerateAccountNumberTest {
+
+    @Test
+    void suggestionForEntryWithMissingValue() throws Exception {
+        Entry entry = new Entry("    _  _  _  _  _  _     _ ","|_||_|| ||_||_   |  |  | _ ","  | _||_||_||_|  |  |  | _|");
+        AccountNumber number = new AccountNumber(entry);
+        List<AccountNumber> suggested = number.suggestedAccountNumbers();
+        List<String> suggestedStrings = new ArrayList<>();
+        for (AccountNumber x: suggested) {
+            suggestedStrings.add(x.toString());
+        }
+        List<String> expected = Arrays.asList("490867715");
+        assertEquals(expected, suggestedStrings);
+    }
+
+    @Test
+    void suggestionForAnotherEntryWithMissingValue() throws Exception {
+        Entry entry = new Entry("    _  _     _  _  _  _  _ "," _| _| _||_||_ |_   ||_||_|","  ||_  _|  | _||_|  ||_| _|");
+        AccountNumber number = new AccountNumber(entry);
+        List<AccountNumber> suggested = number.suggestedAccountNumbers();
+        List<String> suggestedStrings = new ArrayList<>();
+        for (AccountNumber x: suggested) {
+            suggestedStrings.add(x.toString());
+        }
+        List<String> expected = Arrays.asList("123456789");
+        assertEquals(expected, suggestedStrings);
+    }
+}
