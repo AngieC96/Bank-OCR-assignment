20a74caca1a61eaf48c59f62b4ea2691920cc9a2
Angela Carraro <angela_carraro@hotmail.it>
2021-01-06 15:03:33 +0100

    Parametrized test for parseCell finished - Added all tests

--- src/test/java/dssc/assignment/bank/parse/ParseCellTest.java
+++ src/test/java/dssc/assignment/bank/parse/ParseCellTest.java
@@ -1,11 +1,9 @@
 package dssc.assignment.bank.parse;
 
 import dssc.assignment.bank.Cell;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
 
 import java.util.stream.Stream;
 
@@ -15,7 +13,7 @@ public class ParseCellTest {
 
     @ParameterizedTest
     @MethodSource("provideStringsForParsingCells")
-     void zeroCell(String valueCell, String expected) {
+     void parseCellInto(String valueCell, String expected) {
         assertEquals(expected, new Cell(valueCell).toString());
     }
 
@@ -28,29 +26,49 @@ public class ParseCellTest {
                         "   " +
                         "  |" +
                         "  |";
-        return Stream.of(
-                Arguments.of(zeroAsText, "0"),
-                Arguments.of(oneAsText, "1")//,
-                //Arguments.of("  ", "2"),
-                //Arguments.of("not blank", "3")
-        );
-    }
-
-    @Test
-    void oneCell() {
-        String oneAsText =
-                        "   " +
-                        "  |" +
-                        "  |";
-        assertEquals("1", new Cell(oneAsText).toString());
-    }
-
-    @Test
-    void twoCell() {
         String twoAsText =
                         " _ " +
                         " _|" +
                         "|_ ";
-        assertEquals("2", new Cell(twoAsText).toString());
+        String threeAsText =
+                        " _ " +
+                        " _|" +
+                        " _|";
+        String fourAsText =
+                        "   " +
+                        "|_|" +
+                        "  |";
+        String fiveAsText =
+                        " _ " +
+                        "|_ " +
+                        " _|";
+        String sixAsText =
+                        " _ " +
+                        "|_ " +
+                        "|_|";
+        String sevenAsText =
+                        " _ " +
+                        "  |" +
+                        "  |";
+        String eightAsText =
+                        " _ " +
+                        "|_|" +
+                        "|_|";
+        String nineAsText =
+                        " _ " +
+                        "|_|" +
+                        " _|";
+        return Stream.of(
+                Arguments.of(zeroAsText,  "0"),
+                Arguments.of(oneAsText,   "1"),
+                Arguments.of(twoAsText,   "2"),
+                Arguments.of(threeAsText, "3"),
+                Arguments.of(fourAsText,  "4"),
+                Arguments.of(fiveAsText,  "5"),
+                Arguments.of(sixAsText,   "6"),
+                Arguments.of(sevenAsText, "7"),
+                Arguments.of(eightAsText, "8"),
+                Arguments.of(nineAsText,  "9")
+        );
     }
 }
