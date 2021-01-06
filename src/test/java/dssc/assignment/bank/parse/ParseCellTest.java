078077d3c2a7eb2ef8deb9609247dbd46cc77d88
Angela Carraro <angela_carraro@hotmail.it>
2021-01-06 14:38:13 +0100

    Done parametrized test

--- src/test/java/dssc/assignment/bank/parse/ParseCellTest.java
+++ src/test/java/dssc/assignment/bank/parse/ParseCellTest.java
@@ -2,18 +2,39 @@ package dssc.assignment.bank.parse;
 
 import dssc.assignment.bank.Cell;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParseCellTest {
 
-    @Test
-    void zeroCell() {
+    @ParameterizedTest
+    @MethodSource("provideStringsForParsingCells")
+    //@ValueSource(strings = {" _ " + "| |" + "|_|"})
+     void zeroCell(String valueCell, String expected) {
         String zeroAsText =
                         " _ " +
                         "| |" +
                         "|_|";
-        assertEquals("0", new Cell(zeroAsText).toString());
+        assertEquals(expected, new Cell(valueCell).toString());
+    }
+
+    private static Stream<Arguments> provideStringsForParsingCells() {
+        String azeroAsText =
+                        " _ " +
+                        "| |" +
+                        "|_|";
+        return Stream.of(
+                Arguments.of(azeroAsText, "0")//,
+                //Arguments.of("", true),
+                //Arguments.of("  ", true),
+                //Arguments.of("not blank", false)
+        );
     }
 
     @Test
