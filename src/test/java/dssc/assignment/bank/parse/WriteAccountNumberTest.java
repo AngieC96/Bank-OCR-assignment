5704d96c92f01f608610732ff974b9c168fdbeec
Alice <alice.vegliach@gmail.com>
2021-01-06 12:35:15 +0100

    Refactor test writeOnFileWithSuggestions

--- src/test/java/dssc/assignment/bank/parse/WriteAccountNumberTest.java
+++ src/test/java/dssc/assignment/bank/parse/WriteAccountNumberTest.java
@@ -20,10 +20,10 @@ public class WriteAccountNumberTest {
 
     @ParameterizedTest
     @CsvSource({"allZerosEntry, src/test/resources/writeallZerosEntry, '000000000'",
-                "multipleEntries, src/test/resources/writeMultipleEntries, '200800000,999999999 ERR,490867715'",
-                "multipleEntriesWithMissing, src/test/resources/writeMultipleEntriesWithMissing, '200800000,999999999 ERR,49086771? ILL'"})
+                "multipleEntries, src/test/resources/writeMultipleEntries, '200800000;999999999 ERR;490867715'",
+                "multipleEntriesWithMissing, src/test/resources/writeMultipleEntriesWithMissing, '200800000;999999999 ERR;49086771? ILL'"})
     void writeOnFile(String source, String destination, String expected) throws Exception {
-        List<String> expectedAsList = Arrays.asList(expected.split(","));
+        List<String> expectedAsList = Arrays.asList(expected.split(";"));
         URL MultipleLines = BankOcrAcceptanceTest.class.getClassLoader().getResource(source);
         EntryReader reader = new EntryReader(Path.of(MultipleLines.toURI()));
         List<Entry> entries = reader.readEntries();
@@ -39,10 +39,13 @@ public class WriteAccountNumberTest {
 
     }
 
-
-    @Test
-    void MultipleLinesWithSuggestions() throws Exception {
-        URL MultipleLines = BankOcrAcceptanceTest.class.getClassLoader().getResource("multipleEntriesWithMissing");
+    @ParameterizedTest
+    @CsvSource({"multipleEntriesWithMissing, src/test/resources/writeMultipleEntriesWithSuggestions, '200800000;999999999 AMB [899999999, 993999999, 999959999];490867715'",
+                "multipleEntriesWithTwoMissing, src/test/resources/writeMultipleEntriesWithNoSuggestions, '200800000;999999999 AMB [899999999, 993999999, 999959999];?9086771? ILL'",
+                "anotherMultipleEntriesWithMissing, src/test/resources/writeAnotherMultipleEntriesWithMissing, '555555555 AMB [559555555, 555655555];490867715;49?067715 ILL'"})
+    void writeOnFileWithSuggestions(String source, String destination, String expected) throws Exception {
+        List<String> expectedAsList = Arrays.asList(expected.split(";"));
+        URL MultipleLines = BankOcrAcceptanceTest.class.getClassLoader().getResource(source);
         EntryReader reader = new EntryReader(Path.of(MultipleLines.toURI()));
         List<Entry> entries = reader.readEntries();
         List<AccountNumber> numbers = new ArrayList<>();
@@ -50,50 +53,9 @@ public class WriteAccountNumberTest {
             numbers.add(new AccountNumber(entry));
         }
         EntryWriter writer = new EntryWriter(numbers);
-        writer.writeAccountNumbersWithSuggestions(Paths.get("src/test/resources/writeMultipleEntriesWithSuggestions"));
-        List<String> fileLines = Files.readAllLines(Paths.get("src/test/resources/writeMultipleEntriesWithSuggestions"));
-        assertAll(
-                () -> assertEquals("200800000", fileLines.get(0)),
-                () -> assertEquals("999999999 AMB [899999999, 993999999, 999959999]", fileLines.get(1)),
-                () -> assertEquals("490867715", fileLines.get(2))
-        );
+        writer.writeAccountNumbersWithSuggestions(Paths.get(destination));
+        List<String> fileLines = Files.readAllLines(Paths.get(destination));
+        assertEquals(expectedAsList, fileLines);
     }
 
-    @Test
-    void MultipleLinesWithNoSuggestions() throws Exception {
-        URL MultipleLines = BankOcrAcceptanceTest.class.getClassLoader().getResource("multipleEntriesWithTwoMissing");
-        EntryReader reader = new EntryReader(Path.of(MultipleLines.toURI()));
-        List<Entry> entries = reader.readEntries();
-        List<AccountNumber> numbers = new ArrayList<>();
-        for (Entry entry : entries) {
-            numbers.add(new AccountNumber(entry));
-        }
-        EntryWriter writer = new EntryWriter(numbers);
-        writer.writeAccountNumbersWithSuggestions(Paths.get("src/test/resources/writeMultipleEntriesWithNoSuggestions"));
-        List<String> fileLines = Files.readAllLines(Paths.get("src/test/resources/writeMultipleEntriesWithNoSuggestions"));
-        assertAll(
-                () -> assertEquals("200800000", fileLines.get(0)),
-                () -> assertEquals("999999999 AMB [899999999, 993999999, 999959999]", fileLines.get(1)),
-                () -> assertEquals("?9086771? ILL", fileLines.get(2))
-        );
-    }
-
-    @Test
-    void anotherMultipleLinesWithSuggestions() throws Exception {
-        URL MultipleLines = BankOcrAcceptanceTest.class.getClassLoader().getResource("anotherMultipleEntriesWithMissing");
-        EntryReader reader = new EntryReader(Path.of(MultipleLines.toURI()));
-        List<Entry> entries = reader.readEntries();
-        List<AccountNumber> numbers = new ArrayList<>();
-        for (Entry entry : entries) {
-            numbers.add(new AccountNumber(entry));
-        }
-        EntryWriter writer = new EntryWriter(numbers);
-        writer.writeAccountNumbersWithSuggestions(Paths.get("src/test/resources/writeAnotherMultipleEntriesWithMissing"));
-        List<String> fileLines = Files.readAllLines(Paths.get("src/test/resources/writeAnotherMultipleEntriesWithMissing"));
-        assertAll(
-                () -> assertEquals("555555555 AMB [559555555, 555655555]", fileLines.get(0)),
-                () -> assertEquals("490867715", fileLines.get(1)),
-                () -> assertEquals("49?067715 ILL", fileLines.get(2))
-        );
-    }
 }
